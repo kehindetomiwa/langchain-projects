6a35e6e9cbfba49545ca7f99f6a130c60b52c2f4
Kehinde Tomiwa <kehinde.tomiwa-cloud2@outlook.com>
2026-01-11 12:32:35 +0100

    added

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from pyexpat.errors import messages
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from langchain.agents import create_agent
-from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from pydantic import BaseModel, Field
+
+# from langchain.tools import tool
 # from tavily import TavilyClient
 
 # tavily = TavilyClient()
@@ -29,9 +29,24 @@ from langchain_tavily import TavilySearch
 #     return tavily.search(query)
 
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response withh answer and sources"""
+
+    answer: str = Field(description="The final answer from the query")
+    sources: list[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
+
+
 llm = ChatOpenAI()
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
